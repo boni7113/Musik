58fdccf7ddc1749b8b07992ccbe583b17b130b07
boni7113 <bonihericahyadi0@gmail.com>
2025-07-19 03:14:22 +0700

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -1,19 +1,21 @@
-# Credits to Pranav
-
-
 import asyncio
 from pyrogram import Client
 
-
 async def generate_and_send_string_session(api_id, api_hash):
-    async with Client("my_account", api_id=api_id, api_hash=api_hash) as app:
+    async with Client("string", api_id=api_id, api_hash=api_hash) as app:
         string_session = await app.export_session_string()
-        await app.send_message(
-            "me",
-            f"<b>Your Pyrogram String Session:</b>\n\n<code>{string_session}</code>",
-        )
-        print("String session has been sent to your 'Saved Messages'.")
-
+        print("\n==== PYROGRAM SESSION STRING ====\n")
+        print(string_session)
+        print("\n=== COPY DAN SIMPAN ===\n")
+        # Coba kirim ke Saved Messages (jika tidak error)
+        try:
+            await app.send_message(
+                "me",
+                f"<b>Your Pyrogram String Session:</b>\n\n<code>{string_session}</code>",
+            )
+            print("String session juga sudah dikirim ke 'Saved Messages'.")
+        except Exception as e:
+            print(f"Gagal kirim ke Saved Messages: {e}")
 
 if __name__ == "__main__":
     api_id = int(input("Enter your API ID: "))
